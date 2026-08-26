293dbb426d7181a5809dc7574e3e4207302088d8
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-08-25 21:09:06 -0500

    feat(home): assemble Nvet promotional banner source

---
+++ src/data/nvet-home-banner.ts
@@ -0,0 +1,9 @@
+import part1 from './nvet-home-banner-part1';
+import part2 from './nvet-home-banner-part2';
+import part3 from './nvet-home-banner-part3';
+import part4 from './nvet-home-banner-part4';
+import part5 from './nvet-home-banner-part5';
+
+const nvetHomeBannerSrc = `data:image/webp;base64,${part1}${part2}${part3}${part4}${part5}`;
+
+export default nvetHomeBannerSrc;
