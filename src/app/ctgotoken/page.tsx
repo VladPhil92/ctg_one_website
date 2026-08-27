fac073b9914ac4f917188096c3095c1b5aff83a1
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-08-27 15:19:20 -0500

    fix(ctgotoken): use approved CTGO master token render
    * fix(ctgotoken): scope approved master token render to token page
    
    * style(ctgotoken): replace synthetic coin with approved master render
    
    * asset(ctgotoken): add approved CTGO master token render
    
    * fix(ctgotoken): replace corrupt token asset with valid WebP

--- src/app/ctgotoken/page.tsx
+++ src/app/ctgotoken/page.tsx
@@ -3,10 +3,11 @@
 import React from 'react';
 import { PublicPageShell } from '@/components/PublicPageShell';
 import { CtgoTokenSection } from '@/components/sections/CtgoTokenSection';
+import styles from './CtgoTokenPage.module.css';
 
 export default function CtgoTokenPage() {
   return (
-    <PublicPageShell>
+    <PublicPageShell contentClassName={styles.tokenPage}>
       <CtgoTokenSection />
     </PublicPageShell>
   );
